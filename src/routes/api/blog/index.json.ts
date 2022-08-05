46fca53839657f49f9a2a947476bdd06206c1534
HumanBojack <spychalar59@gmail.com>
2022-08-05 22:06:44 +0200

    changed file in case of missing metadata

--- src/routes/api/blog/index.json.ts
+++ src/routes/api/blog/index.json.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { markdownMetaParser } from "$lib/helpers/mardownMetaParser";
-import { getReadTime } from "$lib//helpers/getReadTime";
+import { getReadTime } from "$lib/helpers/getReadTime";
 
 export const GET = async () => {
   const allPostFilesNames = await fs.promises.readdir("src/articles/");
@@ -8,12 +8,10 @@ export const GET = async () => {
   const allPostFiles = await Promise.all(
     allPostFilesNames.map(async (file) => {
       const markdownFile = await fs.promises.readFile(`src/articles/${file}`, 'utf-8')
-      const parsedMeta: { metadata: object, content: string } = await markdownMetaParser(markdownFile);
+      const parsedMeta: { metadata?: object, content: string } = await markdownMetaParser(markdownFile);
       const metadata = parsedMeta.metadata;
 
-      // parsedMeta.metadata.readTime = Math.ceil((parsedMeta.content.length / 5) / 180);
-      // const wordCount: number = parsedMeta.content.split(" ").filter((word) => word != "").length;
-      // parsedMeta.metadata.readTime = Math.ceil(wordCount / 180);
+      if (!metadata) return { name: file.split("."[0]) };
 
       parsedMeta.metadata.readTime = await getReadTime(parsedMeta.content);
 
@@ -25,7 +23,7 @@ export const GET = async () => {
 
     })
   );
-
+  
   const sortedPosts = allPostFiles.sort((a, b) => {
     return (new Date(b.meta.date).getTime() - new Date(a.meta.date).getTime());
   })
