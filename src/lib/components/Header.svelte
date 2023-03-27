6aa221574501696d3778b04e9b764570c42211d2
HumanBojack <spychalar59@gmail.com>
2023-03-27 16:22:35 +0200

    chore: change About section to About me

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,7 +8,7 @@
   <nav>
     <ul>
       <li><a href="/blog">Blog</a></li>
-      <li><a href="/about">About</a></li>
+      <li><a href="/about">About me</a></li>
       <li><a href="/contact">Contact</a></li>
       <li><ToggleDarkMode /></li>
     </ul>
