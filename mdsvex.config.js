0cf5a6268f3edd5293166b7934cefbfcc234c6f1
HumanBojack <spychalar59@gmail.com>
2023-04-16 11:09:46 +0200

    chore: change external link to hard coded svg

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -50,14 +50,27 @@ const mdsvexConfig = defineMDSveXConfig({
       {
         content: {
           type: 'element',
-          tagName: 'img',
+          tagName: 'svg',
           properties: {
-            src: '/icons/external-link.svg',
-            alt: 'External link icon',
+            xmlns: "http://www.w3.org/2000/svg",
+            viewBox: "0 0 24 24",
+            fill: "none",
+            class: "external-link-icon",
           },
-          children: [],
-        },
-        contentProperties: { className: ['external-link-icon'] },
+          children: [
+            {
+              type: 'element',
+              tagName: 'path',
+              properties: {
+                strokeLinecap: "round",
+                strokeLinejoin: "round",
+                strokeWidth: "2",
+                d: "M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              },
+              children: [],
+            }
+          ],
+        }
       }
     ]
   ]
