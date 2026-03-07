eba22d0f415048b5e396aa66e6cca6f943940837
HumanBojack <spychalar59@gmail.com>
2026-03-07 20:03:37 +0100

    feat: add facts voiceclips interractive widget

---
+++ src/lib/components/FactsInteractive.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { playRandomVoiceClip } from '$lib/helpers/interactive';
+</script>
+
+<button on:click={playRandomVoiceClip}>
+	<p>fact</p>
+</button>
+
+<style lang="scss">
+</style>
