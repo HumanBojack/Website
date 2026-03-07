53d7702b2da7a971d9ec929210b0e0c13ba8ae90
HumanBojack <spychalar59@gmail.com>
2026-03-07 20:03:37 +0100

    feat: add facts voiceclips interractive widget

--- src/lib/helpers/interactive.ts
+++ src/lib/helpers/interactive.ts
@@ -9,6 +9,20 @@ const bassSpriteMap = {
   "e": [5108, 515]
 }
 
+const voiceSpriteMap = [
+  [0, 750],
+  [1425, 2056],
+  [4521, 660],
+  [6042, 1225],
+  [8296, 354],
+  [9415, 488],
+  [10880, 1393],
+  [12885, 853],
+  [14479, 798],
+  [15359, 850],
+  [17019, 864],
+  [19106, 696]
+]
 // Look mum I used types in typescript for once !!
 const random = <Type>(list: Array<Type>): Type => {
   return list[Math.floor(Math.random() * list.length)];
@@ -24,6 +38,17 @@ const playRandomBassNote = () => {
   s.play(note);
 }
 
+const playRandomVoiceClip = () => {
+  let line = random(Object.keys(voiceSpriteMap))
+  let s = new Howl({
+    src: ["/audio/sprites/voice.mp3", "/audio/sprites/voice.wav"],
+    sprite: voiceSpriteMap,
+  })
+
+  s.play(line);
+}
+
 export {
   playRandomBassNote,
+  playRandomVoiceClip
 }
