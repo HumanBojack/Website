dfb961f841c9fda19cf465955fd5a31f60b60ea1
HumanBojack <spychalar59@gmail.com>
2026-03-07 18:52:46 +0100

    feat: use howler for sound playing

--- src/lib/helpers/interactive.ts
+++ src/lib/helpers/interactive.ts
@@ -1,11 +1,7 @@
-import { browser } from '$app/environment';
+import { Howl } from 'howler';
 
 // Not pretty but it avoids using fs and I can tweak the scale from code
 const bassNotes = ["e", "g", "a", "b", "d", "eO"]
-let bassAudios: Array<HTMLAudioElement> = []
-if (browser) {
-  bassAudios = bassNotes.map((note) => new Audio(`/audio/bass/${note}.mp3`))
-}
 
 // Look mum I used types in typescript for once !!
 const random = <Type>(list: Array<Type>): Type => {
@@ -13,8 +9,12 @@ const random = <Type>(list: Array<Type>): Type => {
 }
 
 const playRandomBassNote = () => {
-  let s = random(bassAudios)
-  s.play()
+  let note = random(bassNotes)
+  let s = new Howl({
+    src: [`/audio/bass/${note}.mp3`]
+  })
+
+  s.play();
 }
 
 export {
