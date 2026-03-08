428752b932a2c1db5fa03b59943e59e7b9176e5b
HumanBojack <spychalar59@gmail.com>
2026-03-08 03:57:02 +0100

    feat: write facts on a computer monitor

--- src/lib/components/FactsInteractive.svelte
+++ src/lib/components/FactsInteractive.svelte
@@ -18,15 +18,47 @@
 	];
 
 	let button: HTMLElement;
+	let computerText: HTMLElement;
 	let i: Interactible;
+
 	onMount(() => {
-		i = new Interactible('voice', voiceSpriteMap, button);
+		i = new Interactible('voice', voiceSpriteMap, button, () => {
+			computerText.innerText = 'TODO: get a random text from a list';
+		});
 	});
 </script>
 
-	<p>fact</p>
 <button on:click={() => i.interact()} bind:this={button}>
+	<p bind:this={computerText}>
+		Hiiiii!!! I'm Romain / HumanBojack, I'm 24, non binary and I like music and computer stuff c:
+	</p>
+	<img src="/images/interactives/computer.png" />
 </button>
 
 <style lang="scss">
+	button {
+		border: none;
+		background-color: transparent;
+		cursor: pointer;
+		color: var(--background);
+		width: 100%;
+		position: relative;
+
+		overflow: hidden;
+
+		img {
+			max-width: 100%;
+		}
+
+		p {
+			color: var(--text);
+			max-height: 25%;
+			width: 34%;
+			position: absolute;
+			top: 11%;
+			left: 50%;
+			transform: translate(-50%, 0%);
+			overflow: scroll;
+		}
+	}
 </style>
